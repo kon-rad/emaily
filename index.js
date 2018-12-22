30c5a8cc7f80fb502710507ccdcc42bcfc0c7323
konradgnat <konradmgnat@gmail.com>
2018-12-21 19:32:25 -0600

    progress

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 require('./models/User');
 require('./services/passport');
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // thirty days before expiring (in milliseconds)
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect(keys.mongoURI);
 
 require('./routes/authRoutes')(app);
-
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
