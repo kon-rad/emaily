f93af695351410ebffcb3af8e6a089f27f40b0a2
konradgnat <konradmgnat@gmail.com>
2018-12-22 13:13:15 -0600

    survey

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 const app = express();
@@ -23,6 +24,7 @@ mongoose.connect(keys.mongoURI);
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
---
+++ routes/surveyRoutes.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredit = require('../middlewares/requireCredit');
+
+const Survey = mongoose.model('surveys');
+
+module.exports = app => {
+  app.post('/api/surveys', requireLogin, requireCredit, (req, res) => {
+    const { title, body, subject, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    })
+  });
+};
